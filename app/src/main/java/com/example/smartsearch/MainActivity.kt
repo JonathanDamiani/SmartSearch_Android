c65a361f63897652646f1913a721832c6a0f4665
Jonathan Damiani <jdeandamianiapps@gmail.com>
2020-07-22 11:59:36 -0700

    Adding request permissions.

--- app/src/main/java/com/example/smartsearch/MainActivity.kt
+++ app/src/main/java/com/example/smartsearch/MainActivity.kt
@@ -1,12 +1,103 @@
 package com.example.smartsearch
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions()
+        }
+    }
+
+    fun onDetectFromCameraClicked(v: View)
+    {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT ).show()
+    }
+
+    fun onDetectFromFileClicked(v: View)
+    {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT ).show()
+    }
+
+    // Get the required permission from androidManifest
+    private fun getRequiredPermissions(): Array<String?> {
+        return try {
+            val info = this.packageManager
+                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            if (ps != null && ps.isNotEmpty()) {
+                ps
+            } else {
+                arrayOfNulls(0)
+            }
+        } catch (e: Exception) {
+            arrayOfNulls(0)
+        }
+    }
+
+    // Return true if all the necessary permissions are granted
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    // Get all the runtime permission
+    private fun getRuntimePermissions() {
+        val allNeededPermissions = ArrayList<String>()
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    allNeededPermissions.add(permission)
+                }
+            }
+        }
+
+        if (allNeededPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this, allNeededPermissions.toTypedArray(), PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    // Check if the permission is granted.
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    companion object {
+        private const val PERMISSION_REQUESTS = 1
+
+        private fun isPermissionGranted(context: Context, permission: String?): Boolean {
+            if (ContextCompat.checkSelfPermission(context, permission!!)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                return true
+            }
+            return false
+        }
     }
 }
